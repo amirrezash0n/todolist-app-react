611dc130161601eaa79490605e59cd44eccce1e4
amirrezash0n <shourvarziamirreza@gmail.com>
2025-10-25 17:24:44 +0330

    text publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todolist-app/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  base: "/todolist-app-react/",
 });
